f64261a310ceb567c63fe64560113ff24088344a
Kingsly1996 <rkjkumar02@gmail.com>
2026-09-02 00:55:21 +0530

    config added

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export",
+
+  images: {
+    unoptimized: true,
+  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
